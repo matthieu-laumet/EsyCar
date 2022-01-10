ef06ede2576a3550e15ff8ae0bff8aa994f014bd
Matthieu LAUMET <laumetmatthieu@hotmail.fr>
2022-01-10 13:35:22 +0100

    front ok home page

--- app/javascript/component/init_select2.js
+++ app/javascript/component/init_select2.js
@@ -2,7 +2,10 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
+  const select = document.querySelectorAll('.select')
+  if (select) {
+    $('.select2').select2();
+  }
 };
 
 export { initSelect2 };
