fbd9484a2d22a8cc3570837c0a114a46f5b74453
matthieu laumet <laumetmatthieu@hotmail.fr>
2022-01-11 18:14:55 +0100

    add new style

--- app/javascript/component/select_model_from_brand.js
+++ app/javascript/component/select_model_from_brand.js
@@ -1,9 +1,11 @@
 const selectModel = () => {
   const brand = document.querySelector('#car_brand');
+  const model = document.querySelector('div.model-car select ');
 
   if (brand) {
     brand.addEventListener('blur', (event) => {
       console.log(brand.value)
+      console.log(model)
     })
   }
 }
