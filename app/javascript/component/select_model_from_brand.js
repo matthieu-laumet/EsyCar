26e37596f3e1317fabdee1a367d6c6bc372fa853
matthieu laumet <laumetmatthieu@hotmail.fr>
2022-01-14 12:19:24 +0100

    maybe not to merge

--- app/javascript/component/select_model_from_brand.js
+++ app/javascript/component/select_model_from_brand.js
@@ -1,6 +1,6 @@
 const selectModel = () => {
   const brand = document.querySelector('#car_brand');
-  const model = document.querySelector('div.model-car select ');
+  const model = document.querySelector('div.model-car select');
 
   if (brand) {
     brand.addEventListener('blur', (event) => {
