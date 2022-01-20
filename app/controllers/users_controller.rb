2416431c58cd647d059bc590b5a04a52ffb28953
matthieu laumet <laumetmatthieu@hotmail.fr>
2022-01-20 12:35:16 +0100

    it's a trap ! don't merge

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def profile
+    @user = current_user
+    authorize @user
+  end
+
+  def article_params
+    params.require(:user).permit(:last_name, :first_name, :photo)
+  end
+end
